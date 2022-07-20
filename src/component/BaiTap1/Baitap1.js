d2afa35152adf05f6f0e8249074602311265582b
Nguyen Van Nguyen <nguyen.nguyen-van@internal.banvien.com>
2022-07-20 11:42:18 +0700

    create button base

--- src/component/BaiTap1/Baitap1.js
+++ src/component/BaiTap1/Baitap1.js
@@ -1,7 +1,29 @@
 import React from "react";
+import styled from "styled-components";
+import Button from "../Button/Button";
 
 const Baitap1 = () => {
-  return <div>Baitap1</div>;
+  return (
+    <Wrapper>
+      <div className="block">
+        <div>
+          <Button disabled={true} type="secondary" size="sm" />
+          <Button disabled={true} type="primary" size="lg" />
+        </div>
+      </div>
+      <div className="block">asd</div>
+    </Wrapper>
+  );
 };
 
 export default Baitap1;
+
+const Wrapper = styled.div`
+  padding: 50px 170px;
+  display: flex;
+  justify-content: space-between;
+  .block {
+    flex: 1;
+    padding: 10px;
+  }
+`;
---
+++ src/component/Button/Button.js
@@ -0,0 +1,106 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = ({ type, size, disabled }) => {
+  return (
+    <ButtonBase type={type} size={size} disabled={disabled}>
+      Button
+    </ButtonBase>
+  );
+};
+
+export default Button;
+
+const ButtonBase = styled.button`
+  border: none;
+  outline: none;
+  cursor: pointer;
+  border-radius: 4px;
+  font-weight: 700;
+  line-height: 24px;
+  // type
+  ${(props) => {
+    switch (props.type) {
+      case "primary":
+        return `
+          color: white;  
+          border: none;
+          background: #FF0000;
+          &:hover {
+            background: rgba(255, 0, 0, 0.7);
+            /* Dropshadow red */
+            box-shadow: 0px 4px 6px rgba(194, 0, 0, 0.2);            
+          }
+          &:focus {
+            background: #C20000;
+          }
+          &:active {
+            background: #C20000;
+          }
+        `;
+      case "secondary":
+        return `
+          color: #FF0000;
+          border: 1px solid #FF0000;
+          background: white;
+          &:hover {
+            border: 2px solid #FF0000;          
+          }
+          &:focus {
+            border: 2px solid #FF0000;
+          }
+          &:active {
+            border: 2px solid #FF0000;
+          }
+
+        `;
+      default:
+        return `
+          color: white;  
+          border: none;
+        `;
+    }
+  }};
+
+  // size
+  ${(props) => {
+    switch (props.size) {
+      case "sm":
+        return `
+          font-size: 12px;
+          padding: 6px 20px;
+        `;
+      case "lg":
+        return `
+        font-size: 24px;
+        padding: 10px 30px;
+        `;
+      default:
+        return `
+        font-size: 16px;
+        padding: 8px 24px;
+        `;
+    }
+  }};
+
+  // status
+  ${(props) => {
+    switch (props.disabled) {
+      case true:
+        if (props.type === "primary") {
+          return `
+            background: #999999;
+            `;
+        }
+        if (props.type === "secondary") {
+          return `
+            border: 1px solid #999999;
+            color:#999999;
+              `;
+        }
+      default:
+        return `
+        `;
+    }
+  }};
+`;
